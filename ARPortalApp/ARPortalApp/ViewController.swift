8975a80494aadf2cef6a47c97a3f10d01a3523a0
Petar Glisovic <petar.glisovic@gecko.rs>
2019-12-03 22:03:14 +0100

    Add box and grid plane to the scene

--- ARPortalApp/ARPortalApp/ViewController.swift
+++ ARPortalApp/ARPortalApp/ViewController.swift
@@ -12,7 +12,11 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    // MARK: - Outlets
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    // MARK: - Lifecycle functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +24,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,6 +32,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        // Setting plane detection to horizontal so that we are able to detect horizontal planes
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,6 +44,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    // called when touches are detected on the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            
+            // Get location of where we touched on the 2D screen
+            let touchLocation = touch.location(in: sceneView)
+            
+            // hitTest is performed to get the 3D coordinates corresponding to the 2D coordinates that we got from touching the screen. Taht 3D coordinate will only be considered when it is on the existing plane what we detected.
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                
+                let boxScene = SCNScene(named: "art.scnassets/box.scn")
+                
+                if let boxNode = boxScene?.rootNode.childNode(withName: "box", recursively: true) {
+                    
+                    boxNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y + 0.15, z: hitResult.worldTransform.columns.3.z)
+                    
+                    // finally the box is added to the scene
+                    sceneView.scene.rootNode.addChildNode(boxNode)
+                }
+            }
+        }
     }
 
     // MARK: - ARSCNViewDelegate
@@ -57,6 +82,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    // This is delegate method that is called when a horizontal plane is detected
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard anchor is ARPlaneAnchor else {
+            return
+        }
+        
+        // Anchors can be many types. As we are just dealing with horizontal plane detection we need to downcast anchor to ARPlaneAnchor
+        let planeAnchor = anchor as! ARPlaneAnchor
+        
+        // Creating a plane geometry with the help of dimensions we got using plane anchor
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
+        // a node is basically a position
+        let planeNode = SCNNode()
+        
+        // Setting the position of the plane geometry to the position we got using plane anchor.
+        planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+        
+        // When a plane is created, it is created in xy plane instead of xz plane, so we need to rotate it along x axis
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        
+        // Create a material object
+        let gridMaterial = SCNMaterial()
+        
+        // Set material as an image. A material can also be set to a color.
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+        
+        // Assign the material to the plane
+        plane.materials = [gridMaterial]
+        
+        // Assign the position to the plane
+        planeNode.geometry = plane
+        
+        // Add the plane node in our scene
+        node.addChildNode(planeNode)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
